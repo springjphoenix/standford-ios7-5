cf5b41b0992f751eb8be89805de4116714665393
姜春阳 <jiangchunyang@ehaier.com>
2019-01-27 19:09:30 +0800

    UIViewController

--- Attributor/ViewController.m
+++ Attributor/ViewController.m
@@ -9,15 +9,54 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *headline;
 
 @end
 
 @implementation ViewController
+- (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+                                  value:sender.backgroundColor
+                                  range:self.body.selectedRange];
+}
+- (IBAction)outlineBodySelection:(id)sender {
+    [self.body.textStorage addAttributes:@{ NSStrokeWidthAttributeName: @-3,
+                                            NSStrokeColorAttributeName: [UIColor blackColor] }
+                                   range:self.body.selectedRange];
+}
+- (IBAction)unoutlineBodySelection {
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName
+                                     range:self.body.selectedRange];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self usePreferredFonts];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(preferredFontChanged:) name:UIContentSizeCategoryDidChangeNotification
+                                               object:nil];
+}
+
+-(void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
+}
+
+-(void)preferredFontChanged:(NSNotification *)notification {
+    [self usePreferredFonts];
+}
+
+-(void)usePreferredFonts {
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+}
 
 @end
---
+++ README.md
@@ -0,0 +1,1 @@
+### Objective-C ios development
