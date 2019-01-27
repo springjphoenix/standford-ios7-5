c23a47d18734ab2d64dedd4b299b3e5b1f5ca126
姜春阳 <jiangchunyang@ehaier.com>
2019-01-27 14:47:13 +0800

    Initial Commit

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by Spring J Phoenix on 2019/1/27.
+//  Copyright © 2019 Spring J Phoenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by Spring J Phoenix on 2019/1/27.
+//  Copyright © 2019 Spring J Phoenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
